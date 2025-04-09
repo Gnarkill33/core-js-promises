b917b5bdf6c9fb706081f227713c8d76f5db0c37
Gnarkill33 <gnarkill33@gmail.com>
2025-04-10 00:39:47 +0300

    feat: complete queuePromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -138,8 +138,10 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce((chain, promise) => {
+    return chain.then((str) => promise.then((value) => str + String(value)));
+  }, Promise.resolve(''));
 }
 
 module.exports = {
